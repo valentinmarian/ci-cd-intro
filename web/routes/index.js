840ff3df4b79c6964ac38d165c300224fb259743
valentinmarian <60589403+valentinmarian@users.noreply.github.com>
2025-08-20 09:40:59 +1000

    Update index.js with a 207 count for routes

--- web/routes/index.js
+++ web/routes/index.js
@@ -12,7 +12,7 @@ router.get('/ice-flakes', function (req, res, next) {
     .status(201)
     .json({
       resource: 'ice-flakes',
-      count: 208,
+      count: 207,
       shape: 'rectangle'
     })
 })
